3d20c613ee955bd25a9d95d2f02b3153ef81f618
Артём Шевченко <shevchenkoaa@sgu.ru>
2017-09-21 13:55:59 +0400

    Added id for menu

--- static/js/script.js
+++ static/js/script.js
@@ -48,7 +48,7 @@
     });
 
     $(document).ready(function(){
-	     $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+	     $('#nav-icon').click(function(){
 		       $(this).toggleClass('open');
 	     });
     });
